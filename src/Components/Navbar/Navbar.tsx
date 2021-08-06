ccae07e1cd2b5290e4bc3086481236bb52767247
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-06 17:49:04 -0400

    Add material UI

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent, useRef } from 'react'
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function Navbar() {
 	const cityRef = useRef(null);
@@ -6,13 +7,13 @@ export default function Navbar() {
 	const searchCity = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 	}
-	
+
 	return (
 		<header>
 			<h1>Weather App</h1>
 			<form onSubmit={searchCity}>
 				<input type='text' ref={cityRef} placeholder='Search by City' />
-				<button type='submit'></button>
+				<button type='submit'><SearchIcon/></button>
 			</form>
 		</header>
 	)
