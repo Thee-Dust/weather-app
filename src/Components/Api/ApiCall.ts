b1c00c4c8c7cb21a8e8c1a4f48e08dbc5fbe75b6
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-07 21:54:47 -0400

    Add interface for one call api

--- src/Components/Api/ApiCall.ts
+++ src/Components/Api/ApiCall.ts
@@ -1,7 +1,7 @@
 import { Cords } from '../../Utilities/Utilitiles'
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY
 
-export async function getWeather(location: string) {
+export default async function getWeather(location: string) {
   try {
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`);
     const data = await response.json();
@@ -14,7 +14,7 @@ export async function getWeather(location: string) {
 
 const fetchWeather = async(cords: Cords[]) => {
   try{
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cords[0].lat}&lon=${cords[0].lon}&appid=${apiKey}`);
+		const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cords[0].lat}&lon=${cords[0].lon}&appid=${apiKey}&units=imperial`);
     const data = response.json();
     return data
   } catch(e) {
@@ -22,14 +22,25 @@ const fetchWeather = async(cords: Cords[]) => {
   }
 };
 
-export async function getCurrentWeather(city: string) {
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
-    return response.json()
-  } catch(e) {
-    throw Error(e.message);
-  }
-} 
+// export async function getCurrentWeather(city: string) {
+//   try {
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
+//     return response.json()
+//   } catch(e) {
+//     throw Error(e.message);
+//   }
+// } 
+
+// export async function getHourlyForcast(city: string) {
+// 	try {
+// 		const hourResponse = await fetch(`pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${apiKey}`);
+// 		const data = await hourResponse.json()
+// 		console.log(dat)
+// 		return data
+// 	} catch(e) {
+// 		throw Error(e.message)
+// 	}
+// }
 
 // current: { dt: 1627865616, sunrise: 1627814834, sunset: 1627863377, temp: 301.22, feels_like: 304.23, … }
 // daily: (8)[{ … }, { … }, { … }, { … }, { … }, { … }, { … }, { … }]
