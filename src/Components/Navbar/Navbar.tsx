1fa96b4a86c30465cc6b7379f995b4105dd1350a
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-28 16:23:47 -0400

    Add style to input and name of app

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -93,7 +93,7 @@ export default function Navbar({ findCity, tempScale, changeTemp }: { findCity: 
 				<h1>Weather App</h1>
 				<form onSubmit={searchCity} className="search-city">
 					<input type='text' ref={cityRef} placeholder='Search by City' />
-					<button type='submit'><SearchIcon/></button>
+					{/* <button type='submit'><SearchIcon/></button> */}
 				</form>
 				<form className="temp-switch">
 					<FormControlLabel
