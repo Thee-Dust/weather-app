7484dca1b41f1d1308fcbc3478157742cac93666
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-29 12:54:30 -0400

    Style layout for future forecast

--- src/Components/CurrentForecast/CurrentForecast.tsx
+++ src/Components/CurrentForecast/CurrentForecast.tsx
@@ -4,6 +4,7 @@ import { CurrentWeather } from '../../Utilities/Utilitiles';
 // import StarBorderIcon from '@material-ui/icons/StarBorder';
 // import StarIcon from '@material-ui/icons/Star';
 import { getCurrentWeather } from '../Api/ApiCall';
+import './CurrentForecast.scss'
 
 export default function CurrentForecast({ searchedCity, tempScale }: { searchedCity: string, tempScale: string }): ReactElement | null {
 	const [currentWeather, setCurrentWeather] = useState<CurrentWeather | null>(null);
@@ -29,19 +30,19 @@ export default function CurrentForecast({ searchedCity, tempScale }: { searchedC
 	// 		: "imperial";
 	// }
 
-		if(currentWeather?.id) {
-			const today = new Date(currentWeather.dt * 1000);
-			const currentDate = dayjs(today).format('h:mm A');
-			const location = `${currentWeather.name}, ${currentWeather.sys?.country}`;
-			
-			return (
-				<div>
-				<div>
+	if(currentWeather?.id) {
+		const today = new Date(currentWeather.dt * 1000);
+		const currentDate = dayjs(today).format('h:mm A');
+		const location = `${currentWeather.name}, ${currentWeather.sys?.country}`;
+		
+		return (
+			<div className="current-container">
+				<div className="current-forecast">
 					<p>As of {currentDate} in {location}</p>
 					<h3>{currentWeather.main.temp.toFixed(0)}°</h3>
 					<p>{currentWeather.weather[0].main}</p>
 				</div>
-				<div>
+				<div className="current-icon">
 					<img src={`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`} alt={currentWeather.weather[0].description}/>
 				</div>
 			</div>
