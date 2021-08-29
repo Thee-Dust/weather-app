bff4328904f591814b4240f3488c269f933ce4b0
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-29 12:54:30 -0400

    Style layout for future forecast

--- src/Components/HourlyForecast/HourlyForecast.tsx
+++ src/Components/HourlyForecast/HourlyForecast.tsx
@@ -1,14 +1,15 @@
 import React, { ReactElement } from 'react'
 import dayjs from 'dayjs'
 import { Hourly } from '../../Utilities/Utilitiles'
+import './HourlyForecast.scss'
 
 export default function HourlyForcast({ hourlyReport }: { hourlyReport: Hourly[] }): ReactElement {
 	const hourCards = hourlyReport.map((hour, index) => {
 		const date = new Date(hour.dt * 1000);
 		const futureHour = dayjs(date).format('h A');
 		return (
-			<div key={index}>
-				<span>{futureHour}</span>
+			<div key={index} className='hourly-card'>
+				<span>{ index === 0 ? 'Now' : futureHour}</span>
 				<img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`} alt={hour.weather[0].description}/>
 				<span>{hour.temp.toFixed(0)}°</span>
 			</div>
@@ -16,7 +17,7 @@ export default function HourlyForcast({ hourlyReport }: { hourlyReport: Hourly[]
 	})
 
 	return (
-		<div>
+		<div className='hourly-forecast-container'>
 			{hourCards}
 		</div>
 	)
