21dc7b296b69445ac00bf6a1b1931b03bdcd7328
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-30 13:16:44 -0400

    Add a theme in state

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,21 +1,18 @@
 import React, { useEffect, useState, ReactElement } from 'react';
-// import { getCurrentWeather } from '../Api/ApiCall'
 import Navbar from '../Navbar/Navbar';
 import CurrentForecast from '../CurrentForecast/CurrentForecast'
 import FutureForecast from '../FutureForecast/FutureForecast'
-// import { CurrentWeather } from '../../Utilities/Utilitiles';
 import './App.scss';
 
 
 export default function App(): ReactElement {
+	const [ appTheme, setAppTheme ] = useState<string>()
 	const [ searchedCity, setSearchedCity ] = useState<string>(() => {
 		const searchCity = localStorage.getItem('searchedCity');
 		return searchCity !== null
 			? JSON.parse(searchCity)
 			: 'Orlando'
 	});
-	
-
 	
 	const [ tempScale, setTempScale ] = useState<string>(() => {
 		const savedTempScale = localStorage.getItem('tempScale');
@@ -35,21 +32,6 @@ export default function App(): ReactElement {
 		saveSearchedCity()
 	},[searchedCity])
 
-	// const favoriteCity =  (city: string) => {
-	// 	if(favoriteCities.includes(city)){
-	// 		setFavoriteCities(prevState => prevState.filter(favCity => city !== favCity))
-	// 	} else {
-	// 		setFavoriteCities(prevState => [...prevState, city])
-	// 	}
-	// };
-
-  // useEffect(() => {
-	// 	const saveCitiesToStorage = () => {
-	// 		localStorage.setItem('favoriteCities', JSON.stringify(favoriteCities));
-	// 	};
-	// 	saveCitiesToStorage()
-  // },[favoriteCities]);
-
 	const changeTemp = (scale: string) => {
 		setTempScale(scale)
 	}
@@ -61,14 +43,14 @@ export default function App(): ReactElement {
 		saveTempToStorage()
 	},[tempScale])
 
-	const setTheme = () => {
-		
+	const setTheme = (weather: string) => {
+		setAppTheme(weather)
 	}
 
 	return (
-		<main> 
+		<main className={appTheme}> 
 			<Navbar findCity={findCity} tempScale={tempScale} changeTemp={changeTemp}/>
-			<CurrentForecast searchedCity={searchedCity} tempScale={tempScale}/>
+			<CurrentForecast searchedCity={searchedCity} tempScale={tempScale} setTheme={setTheme}/>
 			<FutureForecast searchedCity={searchedCity} tempScale={tempScale}/>
 		</main>
 	);
