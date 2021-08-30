520a4c801193b3fc2a40c9bb52fd19d19a008aec
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-30 14:12:21 -0400

    Change endpoint to HTTPS

--- src/Components/Api/ApiCall.ts
+++ src/Components/Api/ApiCall.ts
@@ -3,7 +3,7 @@ const apiKey = process.env.REACT_APP_WEATHER_API_KEY
 
 export async function getWeather(location: string, tempScale: string) {
   try {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`);
     const data = await response.json();
     const weatherReport = await fetchWeather(data, tempScale);
     return weatherReport
