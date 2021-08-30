287ea837874638cad2308daf1e224607c523cd84
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-30 15:19:09 -0400

    Add test for navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -90,7 +90,7 @@ export default function Navbar({ findCity, tempScale, changeTemp }: { findCity: 
 	return (
 		<header>
 			<div className="app-controls">
-				<h1>Weather App</h1>
+				<h1>The DustStorm</h1>
 				<form onSubmit={searchCity} className="search-city">
 					<input type='text' ref={cityRef} placeholder='Search by City' />
 					{/* <button type='submit'><SearchIcon/></button> */}
