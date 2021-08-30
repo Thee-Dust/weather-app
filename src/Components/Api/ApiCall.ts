fe741a4f8d5c4f17fdf659cc4cfb8250a740bae8
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-30 14:18:25 -0400

    Fix endpoints

--- src/Components/Api/ApiCall.ts
+++ src/Components/Api/ApiCall.ts
@@ -3,7 +3,7 @@ const apiKey = process.env.REACT_APP_WEATHER_API_KEY
 
 export async function getWeather(location: string, tempScale: string) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`);
     const data = await response.json();
     const weatherReport = await fetchWeather(data, tempScale);
     return weatherReport
@@ -14,7 +14,7 @@ export async function getWeather(location: string, tempScale: string) {
 
 const fetchWeather = async(cords: Cords[], tempScale: string) => {
   try{
-		const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cords[0].lat}&lon=${cords[0].lon}&appid=${apiKey}&units=${tempScale}`);
+		const response = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${cords[0].lat}&lon=${cords[0].lon}&appid=${apiKey}&units=${tempScale}`);
     const data = await response.json();
     return data
   } catch(e) {
@@ -24,7 +24,7 @@ const fetchWeather = async(cords: Cords[], tempScale: string) => {
 
 export async function getCurrentWeather(city: string, tempScale: string) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${tempScale}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${tempScale}`);
     return response.json()
   } catch(e) {
     throw Error(e.message);
