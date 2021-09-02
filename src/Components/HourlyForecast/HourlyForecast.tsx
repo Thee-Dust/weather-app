c30ca4428a319fa5a0312dd0da6d4f41089332a9
Dustin-Har <harbaughdustin@yahoo.com>
2021-09-02 15:29:24 -0400

    Add api key to endpoints

--- src/Components/HourlyForecast/HourlyForecast.tsx
+++ src/Components/HourlyForecast/HourlyForecast.tsx
@@ -5,13 +5,14 @@ import './HourlyForecast.scss'
 
 export default function HourlyForcast({ hourlyReport }: { hourlyReport: Hourly[] }): ReactElement {
 	const hourCards = hourlyReport.map((hour, index) => {
+		console.log(hour)
 		const date = new Date(hour.dt * 1000);
 		const futureHour = dayjs(date).format('h A');
 		return (
 			<div key={index} className='hourly-card'>
 				<span>{ index === 0 ? 'Now' : futureHour}</span>
 				<img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`} alt={hour.weather[0].description}/>
-				<span>{hour.temp.toFixed(0)}°</span>
+				<span>{hour.temp.toFixed()}°</span>
 			</div>
 			)
 	})
