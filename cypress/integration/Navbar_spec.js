681ea38185447b03197a0dfbdc55f69c1027f440
Dustin-Har <harbaughdustin@yahoo.com>
2021-09-02 15:29:24 -0400

    Add api key to endpoints

--- cypress/integration/Navbar_spec.js
+++ cypress/integration/Navbar_spec.js
@@ -1,40 +1,42 @@
 describe('NavBar', () => {
+	let apiKey
 	beforeEach(() => {
-		cy.interceptOrlandoCurrentForecastFahrenheit()
-		cy.interceptOrlandoLocation()
-		cy.interceptOrlandoFutureForecastFahrenheit()
+		apiKey = Cypress.env('CYPRESS_APIKEY')
+		cy.interceptOrlandoCurrentForecastFahrenheit(apiKey)
+		cy.interceptOrlandoLocation(apiKey)
+		cy.interceptOrlandoFutureForecastFahrenheit(apiKey)
 		cy.visit('http://localhost:3000')
 	})
-
+	
 	it('Should display app name', () => {
 		cy.get('.app-controls > h1').should('contain', 'The DustStorm')
 	})
 
 	it('Should have a field to search for a city',() => {
+		cy.interceptNYCurrentForecastFahrenheit(apiKey)
+		cy.interceptNYLocation(apiKey)
+		cy.interceptNYFutureForecastFahrenheit(apiKey)
 		cy.get('.search-city > input').type('New York').type('{enter}')
-		cy.interceptNYCurrentForecastFahrenheit()
-		cy.interceptNYLocation()
-		cy.interceptNYFutureForecastFahrenheit()
-		cy.get('.current-forecast > :nth-child(1)').contains('New York')
+			.get('.current-forecast > :nth-child(1)').contains('New York')
 	})
 
 	it('Should change temp to °C from °F', () => {
+		cy.interceptNYCurrentForecastFahrenheit(apiKey)
+		cy.interceptNYLocation(apiKey)
+		cy.interceptNYFutureForecastFahrenheit(apiKey)
 		cy.get('.search-city > input').type('New York').type('{enter}')
-		cy.interceptNYCurrentForecastFahrenheit()
-		cy.interceptNYLocation()
-		cy.interceptNYFutureForecastFahrenheit()
 		.get('.MuiTypography-root').contains('°F')
+		cy.interceptNYCurrentForecastMetric(apiKey)
+		cy.interceptNYFutureForecastMetric(apiKey)
 		.get('.PrivateSwitchBase-input-4').click()
-		cy.interceptNYCurrentForecastMetric()
-		cy.interceptNYFutureForecastMetric()
-			.get('.MuiTypography-root').contains('°C')
+		.get('.MuiTypography-root').contains('°C')
 	})
 
 	it('Should save searched cities', () => {
+		cy.interceptNYCurrentForecastFahrenheit(apiKey)
+		cy.interceptNYLocation(apiKey)
+		cy.interceptNYFutureForecastFahrenheit(apiKey)
 		cy.get('.search-city > input').type('New York').type('{enter}')
-		cy.interceptNYCurrentForecastFahrenheit()
-		cy.interceptNYLocation()
-		cy.interceptNYFutureForecastFahrenheit()
 			.get('.saved-cities').eq(0).contains('New York')
 	})
 })
