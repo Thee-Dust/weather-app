168ea0e0ee876e63f2937d50dfab5ff68c04bb9d
Dustin-Har <harbaughdustin@yahoo.com>
2021-09-10 21:35:26 -0400

    Add styling to weather and home conponent

--- src/Components/Weather/Weather.tsx
+++ src/Components/Weather/Weather.tsx
@@ -2,10 +2,11 @@ import React, { ReactElement } from 'react'
 import CurrentForecast from '../CurrentForecast/CurrentForecast'
 import FutureForecast from '../FutureForecast/FutureForecast'
 import Navbar from '../Navbar/Navbar'
+import './Weather.scss'
 
 export default function Weather({ findCity, tempScale, changeTemp, searchedCity, setTheme }: { findCity: (city: string) => void, tempScale: string, changeTemp: ((scale: string) => void), searchedCity: string, setTheme: (weather: string, time: string) => void }): ReactElement | null{
 	return (
-		<div>
+		<div className='weather'>
 			<Navbar findCity={findCity} tempScale={tempScale} changeTemp={changeTemp} />
 			<CurrentForecast searchedCity={searchedCity} tempScale={tempScale} setTheme={setTheme} />
 			<FutureForecast searchedCity={searchedCity} tempScale={tempScale} />
