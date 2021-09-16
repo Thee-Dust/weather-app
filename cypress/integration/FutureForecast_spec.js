3204a3276c6c83a4b1d2f3e41068a857511ac1ee
Dustin-Har <harbaughdustin@yahoo.com>
2021-09-16 18:16:51 -0400

    Test2

--- cypress/integration/FutureForecast_spec.js
+++ cypress/integration/FutureForecast_spec.js
@@ -6,7 +6,7 @@ describe('Future Forecast', () => {
 		cy.interceptOrlandoCurrentForecastFahrenheit(apiKey)
 		cy.interceptOrlandoFutureForecastFahrenheit(apiKey)
 		cy.visit('http://localhost:3000')
-		cy.log(apikey)
+		cy.log(apiKey)
 		cy.searchOrlando()
 		cy.wait('@OCurrent')
 	})
